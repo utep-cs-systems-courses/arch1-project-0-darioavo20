ab941d1cfc3b8f97c69b67dffc798fc21f072bec
darioavo20 <davazquez5@miners.utep.edu>
2022-02-01 09:28:08 -0700

    Lab 0 Final Code

--- draw_chars.c
+++ draw_chars.c
@@ -13,3 +13,32 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+
+void print_char_5x7(char c)
+{
+  //This first loop prints the array normally so as to show a comparison 
+  c -= 0x20;
+  for (char row = 0; row < 5; row++)
+    {
+      unsigned short rowBits = font_5x7[c][row];
+      for (char col = 0;col < 7; col++)
+	{
+	  unsigned short colMask = 1 << (6-col);
+	  putchar( (rowBits & colMask) ? '*' : ' ');
+	}
+      putchar('\n');
+    }
+  putchar('\n');
+  
+  //This second loop is the actual lab work, i.e. what actually changes the orientation of the char
+  for (char col = 6; col > -1; col--)
+    {
+      unsigned short colMask = 1 << (6-col);
+      for (char row = 0; row < 5; row++)
+	{
+	  unsigned short rowBits = font_5x7[c][row];
+	  putchar( (rowBits & colMask) ? '*' : ' ');
+	}
+      putchar('\n');
+    }
+}
